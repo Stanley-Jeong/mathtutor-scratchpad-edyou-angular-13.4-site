457181a7dbd812547f539947c864c405adad6c6a
sanju <sanju.vdoit@gmail.com>
2024-08-26 23:39:17 +0530

    countdown start ui upadte on main branch

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,10 +1,11 @@
 import { Component, ElementRef, HostListener, Inject, OnDestroy, OnInit, PLATFORM_ID, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
-import { Subject } from 'rxjs';
+import { interval, Subject, Subscription } from 'rxjs';
 import { UserService } from '../service/user.service';
 import { ColorChangeService } from '../service/color-change.service';
 import { isPlatformBrowser } from '@angular/common';
 import { Meta, Title } from '@angular/platform-browser';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
@@ -15,15 +16,24 @@ export class MainComponent implements OnInit,OnDestroy {
   private ngUnsubscribe = new Subject();
   scrollKey: any;
   private isBrowser: boolean;
+  subjectform!: FormGroup;
+
   constructor(private router: Router,private service :UserService ,private service2 : ColorChangeService, @Inject(PLATFORM_ID) private platformId: Object,
-  private titleService: Title, private metaService: Meta) {
+  private titleService: Title, private metaService: Meta,private fb: FormBuilder) {
     this.isBrowser = isPlatformBrowser(this.platformId);
     this.service.getIp().subscribe((res:any)=>{
       console.log(res,"my ip----------------------------------------------")
     })
     // let id = localStorage.getItem('sessionId') 
     // console.log(id,'regettttttttttttttttttttttttttttttttttttttttt')
+    this.subjectform = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]]
+    });
+    
   }
+  
 
   async ngAfterViewInit() {
     this.navigateAndReplaceClass();
@@ -38,13 +48,7 @@ export class MainComponent implements OnInit,OnDestroy {
     }
   }
 
-  ngOnDestroy(): void {
-    // this.ngUnsubscribe.next();
-    this.ngUnsubscribe.complete();
-    if (this.isBrowser) {
-    this.service2.saveScrollPosition(this.scrollKey, window.scrollY);
-    }
-  }
+  
 
   async navigateAndReplaceClass() {
     await this.router.navigateByUrl(this.router.url); // Trigger navigation event
@@ -66,6 +70,11 @@ export class MainComponent implements OnInit,OnDestroy {
     const image3 = document.getElementById('image3') as HTMLImageElement;
     const images: HTMLImageElement[] = [image1, image2, image3];
     this.startImageRotation(images);
+
+    // countdown================
+    this.subscription = interval(1000).subscribe(() => {
+      this.updateCountdown();
+    });
   }
   setTitle(newTitle: string) {
     this.titleService.setTitle(newTitle);
@@ -141,4 +150,75 @@ export class MainComponent implements OnInit,OnDestroy {
   }
   }
 
+  // for countdown
+
+  private subscription: Subscription | undefined;
+  public countdownText: string = '';
+  private targetDate: Date = new Date('2024-09-09T08:00:00')
+  // private targetDate: Date = new Date(new Date().setHours(17, 0, 0, 0));
+
+  days:any
+  hour:any
+  miniute:any
+  second:any
+  isCountdownOver: boolean = false;
+  openForm: boolean = false;
+
+  updateCountdown(): void {
+    const now = new Date().getTime();
+    const distance = this.targetDate.getTime() - now;
+  
+    if (distance < 0) {
+      this.countdownText = 'Countdown is over!';
+      this.isCountdownOver = true;
+      return;
+    }else{
+      this.isCountdownOver = false;
+
+      const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+      const formattedDays = this.formatTimeUnit(days);
+      const formattedHours = this.formatTimeUnit(hours);
+      const formattedMinutes = this.formatTimeUnit(minutes);
+      const formattedSeconds = this.formatTimeUnit(seconds);
+      this.days = formattedDays;
+      this.hour = formattedHours;
+      this.miniute = formattedMinutes;
+      this.second = formattedSeconds;
+    }
+  
+    
+  }
+  
+  private formatTimeUnit(unit: number): string {
+    return unit < 10 ? '0' + unit : unit.toString();
+  }
+
+  openform(){
+    this.openForm = !this.openForm
+  }
+  closeForm(){
+    this.openForm = !this.openForm
+  }
+  submitForm(){
+    if(this.subjectform.valid){
+      console.log('valid')
+    }else{
+      console.log('!valid')
+    }
+  }
+  
+  ngOnDestroy(): void {
+    // this.ngUnsubscribe.next();
+    this.ngUnsubscribe.complete();
+    if (this.isBrowser) {
+    this.service2.saveScrollPosition(this.scrollKey, window.scrollY);
+    }
+
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
 }
