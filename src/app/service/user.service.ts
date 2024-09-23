997231c004f97ef1a0be6f4266270f12895d94f1
raiankit33 <ankit828561@gmail.com>
2024-09-23 18:03:18 +0530

    prod payment off for sc

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -283,6 +283,7 @@ export class UserService {
     return this.http.get('https://api.ipify.org?format=json')
   }
 
+  
   sendwaitlistData(data: any) {
     return this.http.post('https://qzxk7csj32.execute-api.us-west-2.amazonaws.com/Production/Waitlist_added', data)
     //  return this.http.post('https://qzxk7csj32.execute-api.us-west-2.amazonaws.com/Development/Waitlist_added',data)
